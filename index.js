fdce80b7031d783480afc053450229a8c7efb30a
art-gi <artgi@mail.bg>
2022-08-07 14:23:44 +0300

    render index and set up static files

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const PORT = 3000;
 const express = require('express');
+const path = require('path');
+
+const { engine } = require('express-handlebars');
 const app = express();
 
-app.use('/static', express.static('./static'));
+app.use('/static', express.static(path.join(__dirname, './static')));
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('views', './views');
+app.set('view engine', '.hbs');
+
+
 
 app.get('/', (req, res) => {
-    res.status(200).write('views/index');
-    res.end();
-   
+    res.render('index');
+
 })
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
