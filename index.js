84802f008ad5d4d6fef8d21b399f11c44bd260f3
art-gi <artgi@mail.bg>
2022-08-07 12:47:13 +0300

    set up express-handlebars

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const PORT = 3000;
 const express = require('express');
 const app = express();
 
+app.use('/static', express.static('./static'));
+
+app.get('/', (req, res) => {
+    res.status(200).write('views/index');
+    res.end();
+   
+})
+
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
