ca209878788f3aeccec7fcd197a00506fc5ced66
art-gi <artgi@mail.bg>
2022-08-10 17:35:30 +0300

    set env variables

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const PORT = 3000;
 const express = require('express');
 const routes = require('./routes.js');
 const path = require('path');
 
+const config = require('./config/config.json')[process.env.NODE_ENV || "development"];
+
 const app = express();
 
 app.use(express.urlencoded());
+
+
 //require('./config/express.js')(app);
 require('./config/handlebars.js')(app);
 
@@ -13,4 +16,4 @@ app.use('/static', express.static(path.join(__dirname, './static')));
 
 app.use(routes);
 
-app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
+app.listen(config.PORT, console.log(`Server is listening on port ${config.PORT}`));
