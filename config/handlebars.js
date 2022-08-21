7b1b2668e0e1fc1d6b96d4e6f93e5b8a8abd8879
art-gi <artgi@mail.bg>
2022-08-21 18:54:56 +0300

    using path.join to specify the path to a folder views

--- config/handlebars.js
+++
@@ -1,8 +0,0 @@
-function hbs(app) {
-    const { engine } = require('express-handlebars');
-
-    app.engine('.hbs', engine({ extname: '.hbs' }));
-    app.set('views', './views');
-    app.set('view engine', '.hbs');
-};
-module.exports = hbs;
